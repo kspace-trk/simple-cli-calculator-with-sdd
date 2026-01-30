8f94381a98c2bb52d3e0da6e2fae971d6e5988a3
kspace-trk <keigonakao.0825@gmail.com>
2026-01-30 14:24:07 +0900

    feat: createReadlineInterface関数作成

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+import * as readline from "readline/promises";
+
+export const createReadlineInterface = (rl: typeof readline): readline.Interface => {
+  return rl.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+};
